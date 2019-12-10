e5968bc13ecb20524b2cb9bd1510bed1e290ba94
Sara Bolduc <sara.s.bolduc@gmail.com>
2019-12-10 00:03:22 -0800

    updates liri.js - completes runTxtFile() & reformats how info is saved to log.txt

--- liri.js
+++ liri.js
@@ -21,16 +21,9 @@ moment().format();
 // concert-this
 // using: BANDS IN TOWN API via axios
 //==================
-function findConcert(){
+function findConcert(artist){
     //empty array to store retrieved concert info
-    const concertInfo = [];
-
-    //grab & assemble artist/band name
-    //If the user doesn't type an artist/band in, the program will output data for the artist 'Guster'
-    var artist = process.argv.splice(3);
-
-    //need to join movieName and turn it back into a string
-    artist = artist.join().replace(',', '+');
+    var concertInfo = [];
 
     //Bands In Town (BIT) API
     var bandQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
@@ -45,26 +38,30 @@ function findConcert(){
         //artist/band name
         const performer = event.artist.name;
         console.log("Artist: "+ performer);
-        concertInfo.push("Artist: "+ performer);
+        concertInfo.push("\nArtist: "+ performer);
 
         // Name of venue
         const venueName = event.venue.name;
         console.log("Venue Name: "+ venueName);
-        concertInfo.push("Venue Name: "+ venueName);
+        concertInfo.push("\nVenue Name: "+ venueName);
         
         //Location of Venue
         const venueLocation = event.venue.city +","+ event.venue.country;
         console.log("Venue Location: "+ venueLocation);
-        concertInfo.push("Venue Location: "+ venueLocation);
+        concertInfo.push("\nVenue Location: "+ venueLocation);
         
         //Date of Event (formated with moment as MM/DD/YYYY)
         const eventDate = moment(event.datetime).format("MM/DD/YYYY");
         console.log("Event Date: "+ eventDate);
-        concertInfo.push("Event Date: "+ eventDate);
+        concertInfo.push("\nEvent Date: "+ eventDate);
+
+        concertInfo = concertInfo.join().replace(/,/g," ")
 
         //BONUS: Append the above data to file log.txt
-        fs.appendFileSync("log.txt", "\nCONCERT INFO:\n"+concertInfo+"\n", "utf8");
+        fs.appendFileSync("log.txt", "\nCONCERT INFO:"+concertInfo+"\n", "utf8");
+        console.log("-------------------------");
         console.log("Results Saved to log.txt");
+        console.log("-------------------------");
     })
     .catch(function(error) {
         if (error.response) {
@@ -93,17 +90,9 @@ function findConcert(){
 // spotify-this-song
 // using Spotify API
 //==================
-function findSong(){
+function findSong(song){
     //empty array to store retrieved song info
-    const songInfo = [];
-
-    //grab & assemble song title
-    // If no song is provided, default search/result will be "The Sign" by Ace of Base
-    var song = process.argv.splice(3);
-
-    //need to join movieName and turn it back into a string
-    song = song.join().replace(/,/g,' ');
-    //console.log(song);
+    var songInfo = [];
 
     //search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
     //will search by song name and display to following info to terminal & push to songInfo
@@ -117,26 +106,30 @@ function findSong(){
         // // artist(s)
         const musician = songResult.artists[0].name;
         console.log("Artists: "+ musician);
-        songInfo.push("Artists: "+ musician);
+        songInfo.push("\nArtists: "+ musician);
 
         // //song title
         const trackTitle = songResult.name;
         console.log("Track: "+ trackTitle);
-        songInfo.push("Track: "+ trackTitle);
+        songInfo.push("\nTrack: "+ trackTitle);
 
         //album name
         const albumName = songResult.album.name;
         console.log("Album: "+ albumName);
-        songInfo.push("Album: "+ albumName);
+        songInfo.push("\nAlbum: "+ albumName);
 
         //preview link of song
         const songLink = songResult.preveiw_url;
         console.log("Preview Link to Song: "+ songLink);
-        songInfo.push("Preview Link to Song: "+ songLink);
+        songInfo.push("\nPreview Link to Song: "+ songLink);
+
+        songInfo = songInfo.join().replace(/,/g," ");
 
         //BONUS: Append the above data to file log.txt
-        fs.appendFileSync("log.txt", "\nSONG INFO:\n"+songInfo+"\n", "utf8");
+        fs.appendFileSync("log.txt", "\nSONG INFO:"+songInfo+"\n", "utf8");
+        console.log("-------------------------");
         console.log("Results Saved to log.txt");
+        console.log("-------------------------");
     })
     .catch(function(err) {
         console.log(err);
@@ -148,17 +141,10 @@ function findSong(){
 // movie-this
 // using OMDB via axios, api key = trilogy
 //==================
-function findMovie(){
+function findMovie(movieName){
     //empty array to store retrieved movie info
     var movieInfo = [];
-
-    //grab & assemble movie name
-    //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-    var movieName = process.argv.splice(3);
-
-    //need to join movieName and turn it back into a string
-    movieName = movieName.join().replace(',', '+');
-
+;
     // Then run a request with axios to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
@@ -169,46 +155,50 @@ function findMovie(){
             //title of movie
             const movieTitle = response.data.Title;
             console.log("Movie Title: " + movieTitle);
-            movieInfo.push("Movie Title: "+ movieTitle);
+            movieInfo.push("\nMovie Title: "+ movieTitle);
             
             //movie release year
             const releaseYear = response.data.Year;
             console.log("Release Year: " + releaseYear);
-            movieInfo.push("Release Year: " + releaseYear);
+            movieInfo.push("\nRelease Year: " + releaseYear);
             
             //IMDB rating
             const imdbRate = response.data.Ratings[0].Value;
             console.log("IMDB Rating: " + imdbRate);
-            movieInfo.push("IMDB Rating: " + imdbRate);
+            movieInfo.push("\nIMDB Rating: " + imdbRate);
             
             //Rotten Tomatoes Rating
             const rtRate = response.data.Ratings[1].Value;
             console.log("Rotten Tomatoes Rating: " + rtRate);
-            movieInfo.push("Rotten Tomatoes Rating: " + rtRate);
+            movieInfo.push("\nRotten Tomatoes Rating: " + rtRate);
             
             //County of origin
             const country = response.data.Country;
             console.log("Country: " + country);
-            movieInfo.push("Country: " + country);
+            movieInfo.push("\nCountry: " + country);
             
             //language
             const lang = response.data.Language;
             console.log("Language(s): " + lang);
-            movieInfo.push("Language(s): " + lang);
+            movieInfo.push("\nLanguage(s): " + lang);
             
             //plot
             const plot = response.data.Plot;
             console.log("Plot: " + plot);
-            movieInfo.push("Plot: " + plot);
+            movieInfo.push("\nPlot: " + plot);
             
             //actors
             const actors = response.data.Actors
             console.log("Actors: " + actors);
-            movieInfo.push("Actors: " + actors);
+            movieInfo.push("\nActors: " + actors);
+
+            movieInfo = movieInfo.join().replace(/,/g," ")
 
             //BONUS: Append the above data to file log.txt
-            fs.appendFileSync("log.txt", "\nMOVIE INFO:\n"+movieInfo+"\n", "utf8");
+            fs.appendFileSync("log.txt", "\nMOVIE INFO:"+movieInfo+"\n", "utf8");
+            console.log("-------------------------");
             console.log("Results Saved to log.txt");
+            console.log("-------------------------");
         })
         .catch(function(error) {
             if (error.response) {
@@ -245,6 +235,22 @@ function runTxtFile(){
           return console.log(error);
         }
         //do something with data....
+        data = data.split('\n');
+        //console.log(data);
+        data.forEach(element => {
+            var item = element.split(',');
+            if (item[0] === "spotify-this-song"){
+                findSong(item[1]);
+            } else if(item[0] === "movie-this"){
+                findMovie(item[1]);
+            } else if(item[0] === "concert-this"){
+                item[1] = item[1].replace(" ", "+").replace(/"/g, '');
+                findConcert(item[1]);
+            } else{
+                return console.log(item);
+            }
+
+        });
     });
 }
 
@@ -255,11 +261,36 @@ function runTxtFile(){
 const op = process.argv[2];
 
 if(op === "concert-this"){
-    findConcert();
+    //grab & assemble artist/band name
+    //If the user doesn't type an artist/band in, the program will output data for the artist 'Guster'
+    var artist = process.argv.splice(3);
+
+    //need to join movieName and turn it back into a string
+    artist = artist.join().replace(',', '+');
+
+    //run findConcert function
+    findConcert(artist);
 } else if(op === "spotify-this-song"){
-    findSong();
+    //grab & assemble song title
+    // If no song is provided, default search/result will be "The Sign" by Ace of Base
+    var song = process.argv.splice(3);
+
+    //need to join movieName and turn it back into a string
+    song = song.join(" ");
+    //console.log(song);
+
+    //run findSong function
+    findSong(song);
 } else if(op === "movie-this"){
-    findMovie();
+    //grab & assemble movie name
+    //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+    var movieName = process.argv.splice(3);
+
+    //need to join movieName and turn it back into a string
+    movieName = movieName.join().replace(',', '+')
+
+    //run findMovie function
+    findMovie(movieName);
 } else if(op === "do-what-it-says"){
     runTxtFile();
 }
